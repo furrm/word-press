1107310f78644c69dca7c66e6486e26429fbe2d8
furrm <mikefurrhotmail.co.uk>
2013-10-10 08:31:57 +0100

    Init

--- js/controllers/controllers-app.js
+++ js/controllers/controllers-app.js
@@ -1,12 +1,45 @@
 "use strict";
 
-angular.module('controllers-app', []).controller('appCtrl', function ($scope, manifestService) {
+angular.module('controllers-app', []).controller('appCtrl', function ($scope, manifestService, $window, $location) {
     $scope.name = 'appCtrl'
     $scope.serviceName = manifestService.name;
     $scope.appManifest = manifestService.getManifest();
 
-    $scope.authorize = function(){
+    var wordPressVars = {
+        "authorizationEndpoint":"",
+        "tokenEndpoint":"https://public-api.wordpress.com/oauth2/token",
+        "client_id": "23850",
+        "redirect_uri": "http://mobile-cms.azurewebsites.net",
+        "client_secret": "iHgspqA7awWzRYOk6R9SkAdsI6JMstyXHlQkdHlQdz1ysWdomhDWafG0mMqXfbhE",
+        "code": "",
+        "grant_type": "authorization_code"
+    }
+
+    wordPressVars.authorizationEndpoint = "https://public-api.wordpress.com/oauth2/authorize?client_id="
+        + wordPressVars.client_id + "&redirect_uri="
+        + wordPressVars.redirect_uri + "&response_type=code"
+
+
+
+    $scope.wordPress = wordPressVars;
+
+
+
+
+
+    $scope.authorize = function () {
         console.log("Clicked Authorize");
-       $scope.output = manifestService.authorize();
+
+        console.log(wordPressVars);
+//       $scope.output = manifestService.authorize();
+        $window.location.href = wordPressVars.authorizationEndpoint;
+    }
+
+    $scope.requestToken = function () {
+        console.log('Clicked requestToken()');
+        //console.log($location.search('code'));
+        var dataToSend = wordPressVars;
+
+        $scope.output = manifestService.getToken(dataToSend);
     }
 });
